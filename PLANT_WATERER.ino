87c753f914a13a502dbafb55d8cb37f1fc4fedf8
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-07 11:32:34 -0800

    display addressing

--- PLANT_WATERER.ino
+++ PLANT_WATERER.ino
@@ -1,34 +1,53 @@
-
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
 const int relayPin = 2;
-const int sensorPin = A0;
+const int sensorPin = 4;
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 float sensorValue = 0;
 #define SENSOR_READ_TIME 1000
 void setup() {
-// put your setup code here, to run once:
-Serial.begin(9600);
-//begins communication with serial monitor
-pinMode(relayPin, OUTPUT);
-//sets pin 2 to output
-pinMode(sensorPin, INPUT);
-//sets pin A0 to input
-digitalWrite(relayPin, HIGH);
-//turns on relay, in turn turning on pump
-delay(500);
-//runs pump for 1/2 second
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;)
+      ;
+  }
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // begins communication with serial monitor
+  pinMode(relayPin, OUTPUT);
+  // sets pin 2 to output
+  pinMode(sensorPin, INPUT);
+  // sets pin A0 to input
+  digitalWrite(relayPin, HIGH);
+  // turns on relay, in turn turning on pump
+    display.display();
+
+  delay(500);
+  // runs pump for 1/2 second
 }
 
 void loop() {
-// put your main code here, to run repeatedly:
-Serial.print("MOISTURE LEVEL: ");
-//prints "MOISTURE LEVEL: "
-sensorValue = analogRead(sensorPin);
-//sets "sensorValue" to input value from A0
-Serial.print(sensorValue);
-//prints moisture level
-(sensorValue>750)?digitalWrite(relayPin, HIGH):digitalWrite(relayPin, LOW);
 
-Serial.println();
-//prints new line for spacing
-delay(SENSOR_READ_TIME);
-//waits 1 second before re-checking moisture level
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 10);
+  // put your main code here, to run repeatedly:
+  display.println("MOISTURE LEVEL: ");
+  // prints "MOISTURE LEVEL: "
+  sensorValue = analogRead(sensorPin);
+  // sets "sensorValue" to input value from A0
+  display.println(sensorValue);
+  display.display();
+  // prints moisture level
+  (sensorValue > 750) ? digitalWrite(relayPin, HIGH)
+                      : digitalWrite(relayPin, LOW);
+
+  display.println();
+  // prints new line for spacing
+  delay(SENSOR_READ_TIME);
+  // waits 1 second before re-checking moisture level
 }
