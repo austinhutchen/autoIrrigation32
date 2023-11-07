3bf85c7702c5462772a3640e86d98916763b8fce
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-11-07 13:16:57 -0800

    d

--- PLANT_WATERER.ino
+++ PLANT_WATERER.ino
@@ -6,16 +6,11 @@
 const int relayPin = 2;
 const int sensorPin = 4;
 // Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+Adafruit_SSD1306 OLED(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 float sensorValue = 0;
 #define SENSOR_READ_TIME 1000
 void setup() {
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;)
-      ;
-  }
-  // put your setup code here, to run once:
+   // put your setup code here, to run once:
   Serial.begin(115200);
   // begins communication with serial monitor
   pinMode(relayPin, OUTPUT);
@@ -24,29 +19,32 @@ void setup() {
   // sets pin A0 to input
   digitalWrite(relayPin, HIGH);
   // turns on relay, in turn turning on pump
-    display.display();
-
-  delay(500);
   // runs pump for 1/2 second
+ if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed")); 
+  while (true);
+  }
+ OLED.clearDisplay(); // clear display
+
+  OLED.setTextSize(1);         // set text size
+  OLED.setTextColor(WHITE);    // set text color
+  OLED.setCursor(0, 10);  
 }
 
 void loop() {
 
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 10);
-  // put your main code here, to run repeatedly:
-  display.println("MOISTURE LEVEL: ");
+   // put your main code here, to run repeatedly:
+  OLED.println("MOISTURE LEVEL: ");
   // prints "MOISTURE LEVEL: "
   sensorValue = analogRead(sensorPin);
   // sets "sensorValue" to input value from A0
-  display.println(sensorValue);
-  display.display();
+  OLED.println(sensorValue);
+  OLED.display();
   // prints moisture level
   (sensorValue > 750) ? digitalWrite(relayPin, HIGH)
                       : digitalWrite(relayPin, LOW);
 
-  display.println();
+  OLED.println();
   // prints new line for spacing
   delay(SENSOR_READ_TIME);
   // waits 1 second before re-checking moisture level
